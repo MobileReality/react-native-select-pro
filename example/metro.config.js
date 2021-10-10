fcf8c3a775e248108169425e8a5158dd094161bd
irekrog <irek.rog@mobilerelity.pl>
2021-10-10 20:01:49 +0200

    chore: update libraries

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const blacklist = require('metro-config/src/defaults/blacklist');
+const blacklist = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 const pak = require('../../react-native-select-pro/package.json');
 
