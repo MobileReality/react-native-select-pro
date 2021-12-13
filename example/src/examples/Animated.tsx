caf6b1c989655c1f27820154362f75f26a89d64b
Agata Kosior <agata.kosior@tunego.com>
2021-12-13 08:41:55 +0100

    feat: add animation example with different animation durations

--- example/src/examples/Animated.tsx
+++ example/src/examples/Animated.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, Text } from 'react-native';
 import { Select } from '@mobile-reality/react-native-select-pro';
 
 import { DATA } from '../App';
@@ -7,6 +7,9 @@ import { DATA } from '../App';
 export const Animated = () => {
     return (
         <SafeAreaView>
+            <Text style={{ marginBottom: 10 }}>With default animation duration (200 ms)</Text>
+            <Select animated options={DATA} selectControlStyle={{ width: 280, marginBottom: 20 }} />
+            <Text style={{ marginBottom: 10 }}>With custom animation duration (500 ms)</Text>
             <Select
                 animated
                 animationDuration={500}
