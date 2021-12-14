09f0dfe2a018c162426bc4617a873354b8a9aa5d
irekrog <irek.rog@mobilereality.pl>
2021-12-14 21:09:18 +0100

    feat: content changes (wip)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -7,34 +7,24 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
     title: 'React Native Select Pro',
-    tagline: 'Dinosaurs are cool',
     url: 'https://mobilereality.github.io/react-native-select-pro',
     baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'img/favicon.ico',
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
-
+    organizationName: 'Mobile Reality',
+    projectName: 'react-native-select-pro',
     presets: [
         [
             'classic',
-
             /** @type {import('@docusaurus/preset-classic').Options} */
             ({
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    // Please change this to your repo.
-                    editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+                    editUrl: 'https://github.com/MobileReality/react-native-select-pro/website/',
                     remarkPlugins: [
                         [require('@docusaurus/remark-plugin-npm2yarn'), {sync: true}],
                     ],
-                },
-                blog: {
-                    showReadingTime: true,
-                    // Please change this to your repo.
-                    editUrl:
-                        'https://github.com/facebook/docusaurus/edit/main/website/blog/',
                 },
                 theme: {
                     customCss: require.resolve('./src/css/custom.css'),
@@ -50,7 +40,7 @@ const config = {
             navbar: {
                 title: 'React Native Select Pro',
                 logo: {
-                    alt: 'My Site Logo',
+                    alt: 'React Native Select Pro Logo',
                     src: 'img/logo.svg',
                 },
                 items: [
@@ -66,41 +56,11 @@ const config = {
                 style: 'dark',
                 links: [
                     {
-                        title: 'Docs',
+                        title: 'Mobile Reality',
                         items: [
                             {
-                                label: 'Tutorial',
-                                to: '/docs/intro',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'Community',
-                        items: [
-                            {
-                                label: 'Stack Overflow',
-                                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-                            },
-                            {
-                                label: 'Discord',
-                                href: 'https://discordapp.com/invite/docusaurus',
-                            },
-                            {
-                                label: 'Twitter',
-                                href: 'https://twitter.com/docusaurus',
-                            },
-                        ],
-                    },
-                    {
-                        title: 'More',
-                        items: [
-                            {
-                                label: 'Blog',
-                                to: '/blog',
-                            },
-                            {
-                                label: 'GitHub',
-                                href: 'https://github.com/facebook/docusaurus',
+                                label: 'Contact us',
+                                href: 'https://www.mobilereality.pl/en/#footer_contact',
                             },
                         ],
                     },
