43d2135103bb8f10857bf4e5384912f313de5164
kkaplinski <kkaplinskii@gmail.com>
2022-05-04 15:42:10 +0200

    fix: android keyboard issue

--- e2eApp/e2e/basic.e2e.js
+++ e2eApp/e2e/basic.e2e.js
@@ -1,9 +1,10 @@
 describe('Basic', () => {
-    it('should go to Basic Screen and select third option and then clear it ', async () => {
+    it('should go to Basic Screen and select last option and then clear it ', async () => {
         await element(by.text('Basic')).tap();
         await element(by.text('Select...')).tap();
-        await element(by.text('THIRD LABEL')).tap();
-        await expect(element(by.text('THIRD LABEL'))).toExist();
+        await element(by.text('First label')).swipe('up');
+        await element(by.text('Last')).tap();
+        await expect(element(by.text('Last'))).toExist();
         await element(by.label('Clear a chosen option')).atIndex(0).tap();
         await expect(element(by.text('Select...'))).toExist();
     });
--- e2eApp/e2e/multiselect.e2e.js
+++ e2eApp/e2e/multiselect.e2e.js
@@ -1,5 +1,6 @@
 describe('Multi Select', () => {
-    it('should go to Multi Select Screen and select three options and then clear all of them', async () => {
+    it('should go to Multi Select Screen and select four options and then clear all of them', async () => {
+        //Scroll to Multi Select button and press it
         await waitFor(element(by.text('Multi Select')))
             .toBeVisible()
             .whileElement(
@@ -11,21 +12,29 @@ describe('Multi Select', () => {
             )
             .scroll(100, 'down');
         await element(by.text('Multi Select')).tap();
+
+        //Select four elements one by one from the dropdown list
         await element(by.label('Arrow for opening dropdown')).atIndex(0).tap();
         await element(by.text('First label')).tap();
         await element(by.label('Arrow for opening dropdown')).atIndex(0).tap();
         await element(by.text('Second label in options list.')).tap();
         await element(by.label('Arrow for opening dropdown')).atIndex(0).tap();
         await element(by.text('THIRD LABEL')).tap();
+        await element(by.label('Arrow for opening dropdown')).atIndex(0).tap();
+        await element(by.text('----Fourth label----')).tap();
         await expect(element(by.text('First label'))).toExist();
         await expect(element(by.text('THIRD LABEL'))).toExist();
         await expect(
             element(by.text('Second label in options list.')),
         ).toExist();
+        await expect(element(by.text('----Fourth label----'))).toExist();
 
-        await element(by.text('First label')).tap();
-        await element(by.text('Second label in options list.')).tap();
+        //Swipe to the last element in input and clear all of them beginning from the last
+        await element(by.text('First label')).swipe('left');
+        await element(by.text('----Fourth label----')).tap();
         await element(by.text('THIRD LABEL')).tap();
+        await element(by.text('Second label in options list.')).tap();
+        await element(by.text('First label')).tap();
         await expect(element(by.text('Select...'))).toExist();
     });
 });
