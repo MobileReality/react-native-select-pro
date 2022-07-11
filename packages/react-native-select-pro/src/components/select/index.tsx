8cde781cfe1ae0ba056c45928ca03b6ede0813b5
irekrog <irek.rog@mobilereality.pl>
2022-07-11 10:12:12 +0200

    fix: name

--- packages/react-native-select-pro/src/components/select/index.tsx
+++ packages/react-native-select-pro/src/components/select/index.tsx
@@ -70,7 +70,7 @@ export const Select = forwardRef(
             OptionComponent,
             // Custom sources
             customLeftIconSource,
-            // Theme
+            // Colors
             placeholderTextColor = COLORS.GRAY,
             // Accessibility
             selectControlClearOptionA11yLabel,
