32bf39f370281fc74a3cbac0c75e917e185d5114
irekrog <irek.rog@mobilereality.pl>
2022-10-15 22:27:11 +0200

    chore: update dependencies

--- packages/react-native-select-pro/src/helpers/index.ts
+++ packages/react-native-select-pro/src/helpers/index.ts
@@ -1,5 +1,7 @@
+export { getReducedSectionData } from './get-reduced-section-data';
 export { getSize } from './get-size';
 export { heightPercentageToDP } from './height-percentage-to-dp';
 export { isAndroid } from './is-android';
+export { isSectionOptionsType } from './is-section-options-type';
 export { parsePercentageToNumber } from './parse-percentage-to-number';
 export { widthPercentageToDP } from './width-percentage-to-dp';
