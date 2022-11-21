e3b4c04499b87c788504758021a0b3250a8b5c50
Irek Róg <irek.rog@mobilereality.pl>
2022-11-21 19:45:31 +0100

    fix: options list position with page sheet (#131)
    * fix: options list position with presentationStyle=pageSheet
    
    * fix: example
    
    * chore: add changeset

--- packages/react-native-select-pro/src/components/select-provider/index.tsx
+++ packages/react-native-select-pro/src/components/select-provider/index.tsx
@@ -1,4 +1,11 @@
-import React, { ReactNode } from 'react';
+import React, {
+    createContext,
+    ReactNode,
+    useCallback,
+    useRef,
+    useState,
+} from 'react';
+import { InteractionManager, View } from 'react-native';
 import { PortalHost, PortalProvider } from '@gorhom/portal';
 
 import { Portals } from '../../constants/portals';
@@ -25,11 +32,30 @@ export const SelectProvider = ({ children }: Props) => {
     );
 };
 
+export const SelectModalContext = createContext(0);
+
 export const SelectModalProvider = ({ children }: Props) => {
+    const [valueY, setValueY] = useState(0);
+    const ref = useRef<View>(null);
+
+    // this is a necessary to get correct optionsList position for modal with pageSheet presentation style
+    const getModalTopPosition = useCallback(() => {
+        InteractionManager.runAfterInteractions(() => {
+            if (ref.current) {
+                ref.current.measure(
+                    (_x, _y, _width, _height, _pageX, pageY) => {
+                        setValueY(pageY);
+                    },
+                );
+            }
+        });
+    }, []);
+
     return (
-        <>
+        <SelectModalContext.Provider value={valueY}>
+            <View ref={ref} onLayout={getModalTopPosition} />
             <PortalHosts />
             {children}
-        </>
+        </SelectModalContext.Provider>
     );
 };
