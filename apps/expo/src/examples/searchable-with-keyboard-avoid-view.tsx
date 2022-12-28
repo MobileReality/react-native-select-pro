f97f9fa84beebe1ea1cacfb39a48cba114f359ee
kkaplinski <kkaplinskii@gmail.com>
2022-12-28 11:26:31 +0100

    feat: keyboard avoid view

---
+++ apps/expo/src/examples/searchable-with-keyboard-avoid-view.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { KeyboardAvoidingView, Platform, Text } from 'react-native';
+import { Select } from '@mobile-reality/react-native-select-pro';
+
+import { SafeAreaViewWrapper } from '../components/safe-area-view-wrapper';
+import { DATA } from '../constants';
+
+const KeyboardAware = ({ children }: { children: React.ReactNode }) => (
+    <KeyboardAvoidingView
+        style={{
+            flex: 1,
+            justifyContent: 'space-between',
+        }}
+        behavior={Platform.select({ ios: 'padding', android: 'height' })}
+    >
+        {children}
+    </KeyboardAvoidingView>
+);
+
+export const SearchableWithKeyboardAvoidView = () => {
+    return (
+        <SafeAreaViewWrapper>
+            <KeyboardAware>
+                <Text>Some content...</Text>
+                <Text>Some content...</Text>
+                <Text>Some content...</Text>
+                <Select options={DATA} searchable={true} styles={{ width: 250 }} />
+                <Text>Some content...</Text>
+            </KeyboardAware>
+        </SafeAreaViewWrapper>
+    );
+};
